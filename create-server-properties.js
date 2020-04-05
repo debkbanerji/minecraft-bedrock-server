12f9dd7157bc97027392c4e7353d9a15a45643e3
Deb <debkbanerji@gmail.com>
2020-04-04 22:00:29 -0700

    Generating server properties string

--- create-server-properties.js
+++ create-server-properties.js
@@ -7,19 +7,26 @@ const {
   SERVER_PROPERTIES_FIELDS
 } = require('./utils.js')
 
+const getServerPropertiesContentString = (serverProperties) => {
+  return Object.keys(serverProperties).map((property) => `${property}=${serverProperties[property]}`).join('\n\n');
+}
+
 const createServerProperties = () => {
 
   const configFile = fs.readFileSync(CONFIG_FILE_PATH, 'utf8');
   const config = JSON.parse(configFile);
-  const configProperties = config['server-properties'];
-  assert(configProperties, `Expected to find server-properties in ${CONFIG_FILE_PATH}`);
+  const configServerProperties = config['server-properties'];
+  assert(configServerProperties, `Expected to find server-properties in ${CONFIG_FILE_PATH}`);
 
-  actualConfigProperties = Object.keys(configProperties);
+  actualConfigServerPropertyFields = Object.keys(configServerProperties);
 
   // check that the fields are what we expect
-  actualConfigProperties.sort();
+  actualConfigServerPropertyFields.sort();
   SERVER_PROPERTIES_FIELDS.sort();
-  assert.deepEqual(actualConfigProperties, SERVER_PROPERTIES_FIELDS, `Expected fields in config['server-properties'] to be the same as those defined in ./utils.js`)
+  assert.deepEqual(actualConfigServerPropertyFields, SERVER_PROPERTIES_FIELDS, `Expected fields in config['server-properties'] to be the same as those defined in ./utils.js`)
+
+  const content = getServerPropertiesContentString(configServerProperties);
+  console.log(content);
 
   console.log(`Updated ${SERVER_PROPERTIES_FILE_PATH} based on contents of ${CONFIG_FILE_PATH}`);
 }

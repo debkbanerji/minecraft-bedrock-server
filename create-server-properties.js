bdbfd8057be85c12fbc869b11049a31fec06dc8e
Deb <debkbanerji@gmail.com>
2020-04-04 21:54:23 -0700

    Checking property key equality

--- create-server-properties.js
+++ create-server-properties.js
@@ -3,7 +3,8 @@ const assert = require('assert');
 const fs = require('fs');
 const {
   CONFIG_FILE_PATH,
-  SERVER_PROPERTIES_FILE_PATH
+  SERVER_PROPERTIES_FILE_PATH,
+  SERVER_PROPERTIES_FIELDS
 } = require('./utils.js')
 
 const createServerProperties = () => {
@@ -12,6 +13,13 @@ const createServerProperties = () => {
   const config = JSON.parse(configFile);
   const configProperties = config['server-properties'];
   assert(configProperties, `Expected to find server-properties in ${CONFIG_FILE_PATH}`);
+
+  actualConfigProperties = Object.keys(configProperties);
+
+  // check that the fields are what we expect
+  actualConfigProperties.sort();
+  SERVER_PROPERTIES_FIELDS.sort();
+  assert.deepEqual(actualConfigProperties, SERVER_PROPERTIES_FIELDS, `Expected fields in config['server-properties'] to be the same as those defined in ./utils.js`)
 
   console.log(`Updated ${SERVER_PROPERTIES_FILE_PATH} based on contents of ${CONFIG_FILE_PATH}`);
 }
--- utils.js
+++ utils.js
@@ -10,6 +10,32 @@ const WINDOWS_SERVER_LINK = `https://minecraft.azureedge.net/bin-linux/${UNZIPPE
 const LINUX_SERVER_LINK = `https://minecraft.azureedge.net/bin-linux/${UNZIPPED_SERVER_FOLDER_NAME}.zip`;
 const SERVER_PROPERTIES_FILE_NAME = 'server.properties';
 const SERVER_PROPERTIES_FILE_PATH = `${UNZIPPED_SERVER_FOLDER_PATH}/${SERVER_PROPERTIES_FILE_NAME}`;
+const SERVER_PROPERTIES_FIELDS = [
+  'server-name',
+  'gamemode',
+  'difficulty',
+  'allow-cheats',
+  'max-players',
+  'online-mode',
+  'white-list',
+  'server-port',
+  'server-portv6',
+  'view-distance',
+  'tick-distance',
+  'player-idle-timeout',
+  'max-threads',
+  'level-name',
+  'default-player-permission-level',
+  'texturepack-required',
+  'content-log-file-enabled',
+  'compression-threshold',
+  'server-authoritative-movement',
+  'player-movement-score-threshold',
+  'player-movement-distance-threshold',
+  'player-movement-duration-threshold-in-ms',
+  'correct-player-movement'
+]
+
 
 const platform = os.platform();
 
@@ -24,5 +50,6 @@ module.exports = {
   LINUX_SERVER_LINK,
   SERVER_PROPERTIES_FILE_NAME,
   SERVER_PROPERTIES_FILE_PATH,
+  SERVER_PROPERTIES_FIELDS,
   platform
 }

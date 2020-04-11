c9715f33ca0ce7eba1073df90610c31fed69b325
Deb <debkbanerji@gmail.com>
2020-04-11 15:01:27 -0700

    Truncating?

--- backup.js
+++ backup.js
@@ -4,6 +4,9 @@ const assert = require('assert');
 const unzipper = require('unzipper')
 const util = require('util');
 
+const stream = require('stream');
+const pipeline = util.promisify(stream.pipeline);
+
 
 const {
   CONFIG_FILE_PATH,
@@ -19,6 +22,7 @@ async function _createBackupFromFileToCopyLength(fileToCopyLength, backupStartTi
   const backupDirPath = `${BACKUP_FOLDER_PATH}/${backupStartTime}`;
   fs.ensureDirSync(backupDirPath);
   const copyPromises = Object.keys(fileToCopyLength).map(async (fileName) => {
+    const contentLength = fileToCopyLength[fileName];
 
     // Sadly, the 'save query command' doesn't give us the correct path for all
     // files - attempting to fix that now
@@ -28,8 +32,11 @@ async function _createBackupFromFileToCopyLength(fileToCopyLength, backupStartTi
 
     const source = `${SERVER_WORLDS_FOLDER_PATH}/${fileName}`;
     const destination = `${backupDirPath}/${fileName}`;
-    // TODO: Truncate
-    await fs.copy(source, destination);
+    await fs.ensureFile(destination);
+    await pipeline(
+      fs.createReadStream(source,{end: contentLength}),
+      fs.createWriteStream(destination),
+    );
   });
   await Promise.all(copyPromises);
   console.log(`Finished creating backup of server state at ${new Date(backupStartTime*MS_IN_SEC).toLocaleString()}`);
